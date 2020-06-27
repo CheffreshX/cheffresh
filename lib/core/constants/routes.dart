bf53cb94e4def2d1307c46b59311ba994da3d8b2
sherifhasan <sherif.hasan1994@gmail.com>
2020-06-27 17:48:16 +0200

    remove unused files

--- lib/core/constants/routes.dart
+++ lib/core/constants/routes.dart
@@ -1,10 +1,5 @@
 class RoutePaths {
   static const String Login = 'Login Screen';
-  static const String ResetPassword = 'Reset Password Screen';
-  static const String Register = 'Register Screen';
-  static const String Demo = 'Demo Screen';
   static const String Home = 'Home Screen';
-  static const String DeviceInfo = 'Device Info';
-  static const String AddDevice = 'Add Device';
-  static const String EnterDeviceSerial = 'Enter Device Serial';
+
 }
--- lib/core/constants/secrets.dart
+++
@@ -1,1 +0,0 @@
-const String API_ENDPOINT = 'http://52.40.241.0:5000/';
