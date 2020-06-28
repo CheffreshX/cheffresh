0703848a591301e5b51d6e66cebec0bcb1050596
12827629 <michael.k.malak@gmail.com>
2020-06-28 14:40:22 +0200

    fix google map api key for android

--- lib/ui/views/orders/orders_view.dart
+++ lib/ui/views/orders/orders_view.dart
@@ -13,12 +13,12 @@ class DummyOrder {
 
   //Food
   String DishName, Details, DishCategory;
-  NetworkImage FoodPictures;
+  List<String> FoodImages;
   List<String> Tags;
   double Price;
 
   //Food Provider
-  String FoodProviderName, Phone, Address;
+  String FoodProviderName, Phone, Address, FoodProviderImage;
   double FoodProviderRating;
   double Lat, Lng;
 
@@ -26,7 +26,8 @@ class DummyOrder {
     DateTime DateCreated,
     String DishName,
     String Details,
-    NetworkImage FoodPictures,
+    List<String> FoodImages,
+    String FoodProviderImage,
     List<String> Tags,
     String DishCategory,
     double Price,
@@ -40,7 +41,8 @@ class DummyOrder {
     this.DateCreated = DateCreated;
     this.DishName = DishName;
     this.Details = Details;
-    this.FoodPictures = FoodPictures;
+    this.FoodImages = FoodImages;
+    this.FoodProviderImage = FoodProviderImage;
     this.Tags = Tags;
     this.DishCategory = DishCategory;
     this.Price = Price;
@@ -59,8 +61,11 @@ List<DummyOrder> ORDERS = [
     DishName: 'Fish n\' Chips',
     Details:
         'I used fake fish made of soy to preserve the fish in the atlantic ocean. The chips are made of taste-the-difference potatoes from my local Sainsbury’s on the same day. This should keep those fish cravings for those new vegans :). Don’t have these everyday!',
-    FoodPictures: NetworkImage(
-        'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/louis-hansel-shotsoflouis-v3OlBE6-fhU-unsplash.jpg?alt=media&token=75a165e6-7727-489c-aa57-f259e9b5436f'),
+    FoodImages: [
+      'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/meelan-bawjee-A_tPBct4tz8-unsplash.jpg?alt=media&token=609a9559-fe8e-44ae-84e6-530b1d7557eb'
+    ],
+    FoodProviderImage:
+        'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/louis-hansel-shotsoflouis-v3OlBE6-fhU-unsplash.jpg?alt=media&token=75a165e6-7727-489c-aa57-f259e9b5436f',
     Tags: ['Vegan', 'Nut-free'],
     DishCategory: 'Fish n\' Chips',
     Price: 40.0,
@@ -75,9 +80,12 @@ List<DummyOrder> ORDERS = [
     DateCreated: DateTime.parse('1969-07-20 20:18:04Z'),
     DishName: 'Fish n\' Chips',
     Details:
-        'I used fake fish made of soy to preserve the fish in the atlantic ocean. The chips are made of taste-the-difference potatoes from my local Sainsbury’s on the same day. This should keep those fish cravings for those new vegans :). Don’t have these everyday!',
-    FoodPictures: NetworkImage(
-        'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/louis-hansel-shotsoflouis-v3OlBE6-fhU-unsplash.jpg?alt=media&token=75a165e6-7727-489c-aa57-f259e9b5436f'),
+    'I used fake fish made of soy to preserve the fish in the atlantic ocean. The chips are made of taste-the-difference potatoes from my local Sainsbury’s on the same day. This should keep those fish cravings for those new vegans :). Don’t have these everyday!',
+    FoodImages: [
+      'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/meelan-bawjee-A_tPBct4tz8-unsplash.jpg?alt=media&token=609a9559-fe8e-44ae-84e6-530b1d7557eb'
+    ],
+    FoodProviderImage:
+    'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/louis-hansel-shotsoflouis-v3OlBE6-fhU-unsplash.jpg?alt=media&token=75a165e6-7727-489c-aa57-f259e9b5436f',
     Tags: ['Vegan', 'Nut-free'],
     DishCategory: 'Fish n\' Chips',
     Price: 40.0,
@@ -144,63 +152,10 @@ class _OrderCardState extends State<OrderCard> {
               children: <Widget>[
                 Stack(
                   children: <Widget>[
-                    ClipRRect(
-                      borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(5)),
-                      child: Image.network(
-                        'https://firebasestorage.googleapis.com/v0/b/cheffresh-2020.appspot.com/o/meelan-bawjee-A_tPBct4tz8-unsplash.jpg?alt=media&token=609a9559-fe8e-44ae-84e6-530b1d7557eb',
-                        height: ScreenUtil.screenHeightDp / 8,
-                        width: double.maxFinite,
-                        fit: BoxFit.fitWidth,
-                      ),
-                    ),
-                    Positioned(
-                      right: 0.0,
-                      top: 0.0,
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            width: ScreenUtil.screenHeightDp / 10,
-                            height: ScreenUtil.screenHeightDp / 10,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                  width: ScreenUtil().setWidth(3),
-                                  color: CONTRAST_COLOR),
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: widget.order.FoodPictures,
-                              ),
-                            ),
-                          ),
-                          SmoothStarRating(
-                              allowHalfRating: true,
-                              starCount: 5,
-                              rating: widget.order.FoodProviderRating,
-                              size: ScreenUtil().setWidth(15),
-                              isReadOnly: true,
-                              color: ALERT_COLOR,
-                              spacing: 0.0),
-                        ],
-                      ),
-                    ),
+                    FoodVisual(widget: widget),
+                    FoodProviderVisualWidget(widget: widget),
                     if (widget.order.Tags?.isNotEmpty)
-                      Padding(
-                        padding: EdgeInsets.only(left: 5.0, top: 5.0),
-                        child: Container(
-                          width: ScreenUtil.screenWidthDp / 2,
-                          child: Row(
-                            children: <Widget>[
-                              Pill(widget.order.Tags[0],
-                                color: CONTRAST_COLOR,),
-                              SizedBox(width: ScreenUtil().setWidth(5),),
-                              if (widget.order.Tags.length > 1)
-                                Pill(widget.order.Tags[1],
-                                    color: PLACEHOLDER_COLOR),
-                            ],
-                          ),
-                        ),
-                      ),
+                      BuildTags(widget: widget),
                   ],
                 ),
                 Padding(
@@ -214,7 +169,12 @@ class _OrderCardState extends State<OrderCard> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           Text(widget.order.DishName),
-                          Text('£ ' + widget.order.Price.toString()),
+                          Row(
+                            children: <Widget>[
+                              Text('£ ' + widget.order.Price.toString()),
+                              Text('£ ' + widget.order.Price.toString()),
+                            ],
+                          ),
                         ],
                       ),
                     ],
@@ -223,9 +183,9 @@ class _OrderCardState extends State<OrderCard> {
                 if (_isExpanded)
                   Padding(
                     padding: EdgeInsets.symmetric(
-                      horizontal: ScreenUtil().setWidth(10),),
+                      horizontal: ScreenUtil().setWidth(10),
+                    ),
                     child: Column(
-
                       children: <Widget>[
                         Text(widget.order.Details),
                         Text('Address: ' + widget.order.Address),
@@ -251,5 +211,102 @@ class _OrderCardState extends State<OrderCard> {
     setState(() {
       _isExpanded = !_isExpanded;
     });
+  }
+}
+
+class FoodVisual extends StatelessWidget {
+  const FoodVisual({
+    Key key,
+    @required this.widget,
+  }) : super(key: key);
+
+  final OrderCard widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius:
+      BorderRadius.vertical(top: Radius.circular(5)),
+      child: Image.network(
+        widget.order.FoodImages[0],
+        height: ScreenUtil.screenHeightDp / 8,
+        width: double.maxFinite,
+        fit: BoxFit.fitWidth,
+      ),
+    );
+  }
+}
+
+class BuildTags extends StatelessWidget {
+  const BuildTags({
+    Key key,
+    @required this.widget,
+  }) : super(key: key);
+
+  final OrderCard widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 5.0, top: 5.0),
+      child: Container(
+        width: ScreenUtil.screenWidthDp / 2,
+        child: Row(
+          children: <Widget>[
+            Pill(
+              widget.order.Tags[0],
+              color: CONTRAST_COLOR,
+            ),
+            SizedBox(
+              width: ScreenUtil().setWidth(5),
+            ),
+            if (widget.order.Tags.length > 1)
+              Pill(widget.order.Tags[1], color: PLACEHOLDER_COLOR),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FoodProviderVisualWidget extends StatelessWidget {
+  const FoodProviderVisualWidget({
+    Key key,
+    @required this.widget,
+  }) : super(key: key);
+
+  final OrderCard widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0.0,
+      top: 0.0,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil.screenHeightDp / 10,
+            height: ScreenUtil.screenHeightDp / 10,
+            decoration: BoxDecoration(
+              border: Border.all(
+                  width: ScreenUtil().setWidth(3), color: CONTRAST_COLOR),
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(widget.order.FoodProviderImage),
+              ),
+            ),
+          ),
+          SmoothStarRating(
+              allowHalfRating: true,
+              starCount: 5,
+              rating: widget.order.FoodProviderRating,
+              size: ScreenUtil().setWidth(15),
+              isReadOnly: true,
+              color: ALERT_COLOR,
+              spacing: 0.0),
+        ],
+      ),
+    );
   }
 }
